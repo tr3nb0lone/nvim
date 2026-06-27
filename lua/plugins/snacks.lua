9abc2fdab2ec24462cec017bc4dbce3604c38103
tr3nb0lone <tr3nacetate@proton.me>
2026-06-28 00:45:43 +0300

    feat(snacks): minimal dashboard

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -2,12 +2,36 @@ return {
 	"folke/snacks.nvim",
 	priority = 1000,
 	lazy = false,
-	---@type snacks.Config
 	opts = {
 		bigfile = { enabled = true },
-		dashboard = { enabled = true },
+		dashboard = {
+			enabled = true,
+			width = 40,
+			preset = {
+				pick = nil,
+				keys = {
+					{ icon = " 󱐌  ", key = "s", desc = "restore-Session", action = ":AutoSession search <CR>" },
+					{ icon = "   ", key = "r", desc = "Recent(s)", action = ":FzfLua oldfiles <CR>" },
+					{ icon = "   ", key = "H", desc = "checkHealth", action = ":checkhealth <CR>" },
+					{
+						icon = " 󰒲  ",
+						key = "L",
+						desc = "Lazy",
+						action = ":Lazy",
+						enabled = package.loaded.lazy ~= nil,
+					},
+					{ icon = " 󰈆  ", key = "q", desc = "Quit", action = ":qa" },
+				},
+				header = [[ Neovim ]], -- the "banner"
+			},
+			sections = {
+				{ section = "header" },
+				{ section = "keys", gap = 1, padding = 1 },
+				{ section = "startup" },
+			},
+		},
 		input = { enabled = true },
-		picker = { enabled = true },
+		picker = { enabled = false },
 		notifier = { enabled = false },
 		quickfile = { enabled = true },
 		scope = { enabled = true },
