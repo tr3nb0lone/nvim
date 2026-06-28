61d99c25d299e9b2795417047026ba894b88f096
tr3nb0lone <tr3nacetate@proton.me>
2026-06-28 22:02:09 +0300

    stylua

--- lua/utils/neo-tree.lua
+++ lua/utils/neo-tree.lua
@@ -2,34 +2,32 @@
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
 
 return {
-  'nvim-neo-tree/neo-tree.nvim',
-  version = '*',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-    'MunifTanjim/nui.nvim',
-  },
-  cmd = 'Neotree',
-  keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
-  },
-  opts = {
-    filesystem = {
-      filtered_items = {
-        visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
-        hide_dotfiles = false,
-        hide_gitignored = true,
-        hide_by_name = {
-          '.git',
-        },
-      },
-      window = {
-        position = 'right',
-        width = 28,
-        mappings = {
-          ['\\'] = 'close_window',
-        },
-      },
-    },
-  },
+	"nvim-neo-tree/neo-tree.nvim",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+	},
+	cmd = "Neotree",
+	keys = {
+		{ "\\", ":Neotree reveal<CR>", desc = "NeoTree reveal", silent = true },
+	},
+	opts = {
+		filesystem = {
+			filtered_items = {
+				visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+				hide_dotfiles = false,
+				hide_gitignored = true,
+				hide_by_name = {
+					".git",
+				},
+			},
+			window = {
+				position = "right",
+				width = 28,
+				mappings = {
+					["\\"] = "close_window",
+				},
+			},
+		},
+	},
 }
